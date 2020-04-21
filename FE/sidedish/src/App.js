62d8b3e0b36cb65f2b59bbf083426770cb5bccda
Reese Kim <reesekimm@gmail.com>
2020-04-21 21:44:59 +0900

    Feat: Carousel 라이브러리 설치 및 테스트

--- FE/sidedish/src/App.js
+++ FE/sidedish/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import LibraryCarousel from "./components/LibraryCarousel";
+import "react-multi-carousel/lib/styles.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<LibraryCarousel />
+		</div>
+	);
 }
 
 export default App;
