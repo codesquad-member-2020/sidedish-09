6ced838aefba0e64970d531499465285e43de3cd
Reese Kim <reesekimm@gmail.com>
2020-04-22 18:28:41 +0900

    Refactor: Carousel 클래스형 컴포넌트 -> 함수형 컴포넌트 변환

--- FE/sidedish/src/components/carouselLibR/Carousel.jsx
+++ FE/sidedish/src/components/carouselLibR/Carousel.jsx
@@ -6,48 +6,40 @@ import "./style.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default class Carousel extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: mockData.body,
-			loading: true,
-			error: null,
-		};
-	}
+const Carousel = () => {
+	const SETTINGS = {
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+	};
 
-	render() {
-		var settings = {
-			infinite: true,
-			speed: 500,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-		};
-
-		const { data } = this.state;
-		console.log(data);
-
-		return (
-			<div className="container">
-				<div>
-					<h2>밑반찬</h2>
-					<p>언제 먹어도 든든한 반찬</p>
-				</div>
-				<Slider {...settings}>
-					{data.map((itemData) => (
-						<CarouselItem
-							key={itemData.detail_hash}
-							image={itemData.image}
-							alt={itemData.alt}
-							title={itemData.title}
-							description={itemData.description}
-							normalPrice={itemData.n_price}
-							specialPrice={itemData.s_price}
-							badge={itemData.badge}
-						/>
-					))}
-				</Slider>
+	return (
+		<div className="container">
+			<div>
+				<h2>밑반찬</h2>
+				<p>언제 먹어도 든든한 반찬</p>
 			</div>
-		);
-	}
-}
+			<Slider {...SETTINGS}>
+				{mockData.body.map(
+					({ detail_hash, image, alt, title, description, n_price, s_price, badge }) => {
+						return (
+							<CarouselItem
+								key={detail_hash}
+								image={image}
+								alt={alt}
+								title={title}
+								description={description}
+								normalPrice={n_price}
+								specialPrice={s_price}
+								badge={badge}
+							/>
+						);
+					}
+				)}
+			</Slider>
+		</div>
+	);
+};
+
+export default Carousel;
