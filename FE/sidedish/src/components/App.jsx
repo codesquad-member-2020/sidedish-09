0b42f8e3f7ff302581d30ba0ebf64d4589d68cf8
Reese Kim <reesekimm@gmail.com>
2020-04-22 18:49:59 +0900

    Feat: styled-reset 적용

--- FE/sidedish/src/components/App.jsx
+++ FE/sidedish/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Carousel from "./carouselLibR/Carousel";
+import GlobalStyles from "../style/GlobalStyle";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyles />
 			<Carousel />
 		</div>
 	);
