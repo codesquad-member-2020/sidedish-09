69e9cebbb5aaa87d8c8145ebd13b02185d563c23
Hu2y <sanghun_@daum.net>
2020-04-23 15:20:54 +0900

    Feat: header컴포넌트에 LogoNav 컴포넌트 연결 #4

--- FE/sidedish/src/components/header/Header.jsx
+++ FE/sidedish/src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LinkNav from './LinkNav'
+import LogoNav from './LogoNav'
 
 const Header = () => {
   return (
     <>
       <LinkNav />
+      <LogoNav />
     </>
   );
 };
