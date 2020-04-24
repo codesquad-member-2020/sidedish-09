2128c127bb21e83705142e68701730aa52f9386a
Reese Kim <reesekimm@gmail.com>
2020-04-24 16:42:49 +0900

    Feat: 썸네일 뷰어 기능 구현 Resolve #16

--- FE/sidedish/src/components/product/ThumbnailViewer.jsx
+++ FE/sidedish/src/components/product/ThumbnailViewer.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThumbnailContainer, Current, List, Thumbnail } from "style/product/ThumbnailViewer";
 
 const ThumbnailViewer = ({ thumbnails }) => {
+	const [current, setCurrent] = useState(thumbnails[0]);
+
+	const handleCurrentThumbnail = (e) => {
+		setCurrent(e.target.src);
+	};
+
 	return (
 		<ThumbnailContainer>
 			<Current>
-				<img src={thumbnails[0]} />
+				<img src={current} />
 			</Current>
 			<div>
 				<List>
 					{thumbnails.map((url) => (
 						<Thumbnail>
-							<img src={url} />
+							<img src={url} onMouseEnter={handleCurrentThumbnail} />
 						</Thumbnail>
 					))}
 				</List>
