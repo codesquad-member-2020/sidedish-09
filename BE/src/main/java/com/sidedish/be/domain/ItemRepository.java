124691f20418916c19e2d6ae4080c59881060eb7
kyungrae <rlarudfo93@gmail.com>
2020-04-27 17:47:32 +0900

    feat: Delivery 객체와 Sale 객체 조회 기능 구현 [#30]

--- BE/src/main/java/com/sidedish/be/domain/ItemRepository.java
+++ BE/src/main/java/com/sidedish/be/domain/ItemRepository.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -20,6 +21,16 @@ import java.util.stream.Collectors;
 public class ItemRepository {
 
     private final String SELECT_ITEM_BY_HASH =
+            "SELECT i.id, i.hash, i.image, i.title, i.description, i.n_price, i.delivery_fee, i.delivery_date, " +
+                    "GROUP_CONCAT(DISTINCT d.delivery_id), GROUP_CONCAT(DISTINCT s.sale_id), " +
+                    "GROUP_CONCAT(DISTINCT t.url), GROUP_CONCAT(DISTINCT dI.url) FROM item i " +
+                    "LEFT OUTER JOIN item_delivery d on i.id = d.item_id " +
+                    "LEFT OUTER JOIN item_sale s on i.id = s.item_id " +
+                    "LEFT OUTER JOIN thumbnail t on i.id = t.item_id " +
+                    "LEFT OUTER JOIN detailImage dI on i.id = dI.item_id " +
+                    "WHERE i.hash = :hash GROUP BY i.id";
+
+    private final String SELECT_ITEMS_BY_HASH =
             "SELECT i.id, i.hash, i.image, i.title, i.description, i.n_price, i.delivery_fee, i.delivery_date, " +
                     "GROUP_CONCAT(DISTINCT d.delivery_id), GROUP_CONCAT(DISTINCT s.sale_id), " +
                     "GROUP_CONCAT(DISTINCT t.url), GROUP_CONCAT(DISTINCT dI.url) FROM item i " +
@@ -71,9 +82,13 @@ public class ItemRepository {
                 .build();
     };
 
+    public Optional<Item> findByHash(String hash) {
+        SqlParameterSource parameterSource = new MapSqlParameterSource().addValue("hash", hash);
+        return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_ITEM_BY_HASH, parameterSource, itemRowMapper));
+    }
 
     public List<Item> findByHash(List<String> hash) {
         SqlParameterSource parameterSource = new MapSqlParameterSource().addValue("hash", hash);
-        return jdbcTemplate.query(SELECT_ITEM_BY_HASH, parameterSource, itemRowMapper);
+        return jdbcTemplate.query(SELECT_ITEMS_BY_HASH, parameterSource, itemRowMapper);
     }
 }
