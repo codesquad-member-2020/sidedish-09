785264c3716a7b12d38e4d1c1685fa217a5f2a80
kyungrae <rlarudfo93@gmail.com>
2020-04-29 01:57:38 +0900

    [BE] deploy 스크립트 빌드 로직 수정
    remote repository에 변경사항이 있을 때 빌드하도록 변경헀다.

--- deploy.sh
+++ deploy.sh
@@ -6,27 +6,27 @@ LOCAL=$(git rev-parse HEAD)
 NGINX_STATIC_DIR=/var/www/html
 TOMCAT_DIR=$HOME/tomcat
 
+./teardown.sh
+
 echo ">>> check github repository"
 if [ $REMOTE != $LOCAL ]; then
     echo ">>> github pull"
     git pull
+
+    echo ">>> Delete react file and webapps file"
+    rm -rf $NGINX_STATIC_DIR/*
+    rm -rf $TOMCAT_DIR/webapps/*
+
+    echo ">>> Build front-end"
+    npm run build --prefix FE/sidedish
+    mv $BASE_DIR/FE/sidedish/build/* $NGINX_STATIC_DIR/
+
+    echo ">>> Build back-end"
+    cd BE
+    ./gradlew bootWar
+    mv $BASE_DIR/BE/build/libs/be-0.0.1-SNAPSHOT.war $BASE_DIR/BE/build/libs/ROOT.war
+    mv $BASE_DIR/BE/build/libs/ROOT.war $TOMCAT_DIR/webapps/
 fi
-
-./teardown.sh
-
-echo ">>> Delete react file and webapps file"
-rm -rf $NGINX_STATIC_DIR/*
-rm -rf $TOMCAT_DIR/webapps/*
-
-echo ">>> Build front-end"
-npm run build --prefix FE/sidedish
-mv $BASE_DIR/FE/sidedish/build/* $NGINX_STATIC_DIR/
-
-echo ">>> Build back-end"
-cd BE
-./gradlew bootWar
-mv $BASE_DIR/BE/build/libs/be-0.0.1-SNAPSHOT.war $BASE_DIR/BE/build/libs/ROOT.war
-mv $BASE_DIR/BE/build/libs/ROOT.war $TOMCAT_DIR/webapps/
 
 echo ">>> Start nginx and tomcat"
 service nginx start
