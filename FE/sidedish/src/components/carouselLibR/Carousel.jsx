3fe1633ad9aa2a95e340996d4c8d46c998ca46f6
Reese Kim <reesekimm@gmail.com>
2020-04-28 15:51:04 +0900

    Feat: Carousle 서버 연동

--- FE/sidedish/src/components/carouselLibR/Carousel.jsx
+++ FE/sidedish/src/components/carouselLibR/Carousel.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CarouselHeader from "./CarouselHeader";
 import Slider from "react-slick";
+import Loader from "../loader/Loader";
 import CarouselItem from "./CarouselItem";
-import mockData from "assets/mockData.js";
+import axios from "axios";
 import "style/carouselLibR/Buttons.css";
 import { CarouselContainer } from "style/carouselLibR/Carousel";
 import { ProductProvider } from "contexts/productContext";
+
+const loaderContainerStyle = {
+	width: "100%",
+	height: "510px",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+};
 
 const SETTINGS = {
 	infinite: true,
@@ -15,43 +24,69 @@ const SETTINGS = {
 	initialSlide: 0,
 };
 
-const Carousel = () => (
-	<>
-		<CarouselHeader title={mockData.title} subTitle={mockData.description} />
-		<ProductProvider>
-			<CarouselContainer>
-				<Slider {...SETTINGS}>
-					{mockData.items.map(
-						({
-							detail_hash,
-							image,
-							alt,
-							delivery_type,
-							title,
-							description,
-							n_price,
-							s_price,
-							badge,
-						}) => {
-							return (
-								<CarouselItem
-									key={detail_hash}
-									image={image}
-									alt={alt}
-									deliveryType={delivery_type}
-									title={title}
-									description={description}
-									originalPrice={n_price}
-									sellingPrice={s_price}
-									badge={badge}
-								/>
-							);
-						}
-					)}
-				</Slider>
-			</CarouselContainer>
-		</ProductProvider>
-	</>
-);
+const Carousel = () => {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState(null);
+
+	const getCarouselData = async () => {
+		try {
+			const response = await axios.get(`${process.env.REACT_APP_BASE_URL}categories?type=carousel`);
+			const { status, data } = response;
+			if (status === 200) {
+				setData(data.find((carousel) => carousel.title === "밑반찬"));
+			}
+		} catch (err) {
+			console.log(err);
+		}
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		getCarouselData();
+	}, []);
+
+	return loading ? (
+		<div style={loaderContainerStyle}>
+			<Loader />
+		</div>
+	) : (
+		<div>
+			<CarouselHeader title={data.title} subTitle={data.description} />
+			<ProductProvider>
+				<CarouselContainer>
+					<Slider {...SETTINGS}>
+						{data.items.map(
+							({
+								detail_hash,
+								image,
+								alt,
+								delivery_type,
+								title,
+								description,
+								n_price,
+								s_price,
+								badge,
+							}) => {
+								return (
+									<CarouselItem
+										key={detail_hash}
+										image={image}
+										alt={alt}
+										deliveryType={delivery_type}
+										title={title}
+										description={description}
+										originalPrice={n_price}
+										sellingPrice={s_price}
+										badge={badge}
+									/>
+								);
+							}
+						)}
+					</Slider>
+				</CarouselContainer>
+			</ProductProvider>
+		</div>
+	);
+};
 
 export default Carousel;
